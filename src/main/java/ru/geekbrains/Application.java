c0e945b16a34fe9798ffc8ccb15353ca4052f92d
Nezhdanov Sergei <tsebom@yandex.ru>
2022-04-17 12:05:26 +0300

    first_commit

--- src/main/java/ru/geekbrains/Application.java
+++ src/main/java/ru/geekbrains/Application.java
@@ -1,4 +1,24 @@
 package ru.geekbrains;
 
-public class Aplication {
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import ru.geekbrains.config.AppConfig;
+import ru.geekbrains.services.ConsoleMenage;
+import ru.geekbrains.spring.Product;
+import ru.geekbrains.spring.ProductRepository;
+
+public class Application {
+    public static void main(String[] args) {
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        ProductRepository productRepository = context.getBean(ProductRepository.class);
+
+        productRepository.addProduct(context.getBean(Product.class, 1, "Nokia", 5.67));
+        productRepository.addProduct(context.getBean(Product.class, 2, "Samsung", 6.67));
+        productRepository.addProduct(context.getBean(Product.class, 3, "Bell", 7.67));
+        productRepository.addProduct(context.getBean(Product.class, 4, "Sony", 8.67));
+        productRepository.addProduct(context.getBean(Product.class, 5, "LG", 9.67));
+
+        context.getBean(ConsoleMenage.class).menage();
+    }
 }
--- src/main/java/ru/geekbrains/config/AppConfig.java
+++ src/main/java/ru/geekbrains/config/AppConfig.java
@@ -1,2 +1,21 @@
-package ru.geekbrains.config;public class AppConfig {
+package ru.geekbrains.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import ru.geekbrains.services.Cart;
+import ru.geekbrains.services.ConsoleMenage;
+
+@Configuration
+@ComponentScan("ru.geekbrains.spring")
+public class AppConfig {
+    @Bean
+    public Cart getCart() {
+        return new Cart();
+    }
+
+    @Bean
+    public ConsoleMenage getConsole() {
+        return new ConsoleMenage();
+    }
 }
